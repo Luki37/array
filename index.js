f763de301148b4e0cd42d45fd67797233e0fedad
Luki37 <286344374+Luki37@users.noreply.github.com>
2026-06-25 23:22:05 +0200

    add tasks

---
+++ index.js
@@ -0,0 +1,7 @@
+/*array erstellen */
+/*elemente dazufügen */
+/*elemente nach position entfernen*/
+/*elemente nach inhalt entfernen*/
+/*anzeigen lassen (evtl. automatisch auf liste)*/
+/* array multiplizieren (mit 2, evtl option beliebige zahl)*/
+/*summe des array anzeigen*/
