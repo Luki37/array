22f06ef7cb52a378c73073a51b41a481364e7f35
Luki37 <286344374+Luki37@users.noreply.github.com>
2026-06-26 13:23:37 +0200

    add funct. sum and multi

--- index.js
+++ index.js
@@ -9,47 +9,48 @@ const addbtn = document.getElementById("content");
 const delidbtn = document.getElementById("delid");
 const delcontbtn = document.getElementById("delcont");
 
-const showbtn = document.getElementById("show");
-const multibtn = document.getElementById("multi");
-const sumbtn = document.getElementById("sum");
-
 const showList = document.getElementById("list");
 const result = document.getElementById("result");
-
-/* function renderList() {
-  showList.innerHTML = "";
-
-  list.forEach((number) => {
-    listElement.textContent = number;
-    showList.appendChild(listElement);
-  });
-} */
 
 /*elemente dazufügen */
 function content() {
   list.push(parseFloat(add.value));
-  console.log(list);
   add.value = "";
-  console.log(list);
   showList.innerHTML = "";
 }
 
 /*elemente nach position entfernen*/
+
 /*elemente nach inhalt entfernen*/
+
 /*anzeigen lassen (evtl. automatisch auf liste)*/
 
 function show() {
-  showList.innerHTML = "";
-
   list.forEach((number) => {
     const listElement = document.createElement("li");
     listElement.textContent = number;
     showList.appendChild(listElement);
   });
 }
+
 /* array multiplizieren (mit 2, evtl option beliebige zahl)*/
 
-function multi() {}
+function multi() {
+  showList.innerHTML = "";
+  list.map((number) => {
+    const doubled = number * 2;
+    const listElement = document.createElement("li");
+    listElement.textContent = doubled;
+    showList.appendChild(listElement);
+    return doubled;
+  });
+}
+
 /*summe des array anzeigen*/
 
-function sum() {}
+function sum() {
+  const resultSum = list.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue;
+  });
+  result.textContent = "Die Summe ist: " + resultSum;
+}
