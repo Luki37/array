c556873d871209e75812a2158184cdc32559e849
Luki37 <286344374+Luki37@users.noreply.github.com>
2026-06-27 10:26:31 +0200

    creat list2 for better mutliply

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /*array und variable erstellen */
 const list = [];
+let list2 = [];
 
 const add = document.getElementById("input1");
 const position = document.getElementById("input2");
@@ -10,7 +11,6 @@ const result = document.getElementById("result");
 
 /*elemente dazufügen */
 function content() {
-  /*alert einbauen wenn etwas anderes als eine zahl eingeeben wird*/
   list.push(parseFloat(add.value));
   clearInput();
   showList.innerHTML = "";
@@ -46,24 +46,29 @@ function show() {
     const listElement = document.createElement("li");
     listElement.textContent = number;
     showList.appendChild(listElement);
+    list2 = [];
+    result.textContent = "";
   });
 }
 
 /* array multiplizieren (mit 2, evtl option beliebige zahl)*/
 function multi() {
   showList.innerHTML = "";
-  list.map((number) => {
+  const activeList = list2.length > 0 ? list2 : list;
+  list2 = activeList.map((number) => {
     const doubled = number * 2;
     const listElement = document.createElement("li");
     listElement.textContent = doubled;
     showList.appendChild(listElement);
+    result.textContent = "";
     return doubled;
   });
 }
 
 /*summe des array anzeigen*/
 function sum() {
-  const resultSum = list.reduce((previousValue, currentValue) => {
+  const activeList = list2.length > 0 ? list2 : list;
+  const resultSum = activeList.reduce((previousValue, currentValue) => {
     return previousValue + currentValue;
   });
   result.textContent = "Die Summe ist: " + resultSum;
